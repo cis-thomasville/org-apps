96001c438457654159a9ef52709da20d3aee5572
MarkMutt <lleasy8@gmail.com>
2025-08-10 20:48:21 -0400

    updated db export

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,3 +1,9 @@
-const db = {};
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+import { config } from "dotenv";
+
+config({ path: ".env" });
+
+const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 export { db };
